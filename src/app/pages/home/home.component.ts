caab3fd67b657db98e1c0785869447edab1e8494
Alléxia Alcantara <allexia@henkoti.com.br>
2022-04-05 11:50:29 -0300

    ajuste responsivo tela de Registro

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,10 @@
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { setTheme } from 'ngx-bootstrap/utils';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { ptBrLocale } from 'ngx-bootstrap/locale';
+defineLocale('pt-br', ptBrLocale);
 
 @Component({
   selector: 'app-home',
@@ -11,8 +15,10 @@ export class HomeComponent implements OnInit {
 
   cards = [];
   isCollapsed = true;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    localeService: BsLocaleService) {
     setTheme('bs5');
+    localeService.use('pt-br')
   }
 
   ngOnInit(): void {
--- src/app/pages/perfil/immobile/detail-of-immobile/detail-of-immobile.component.ts
+++ src/app/pages/perfil/immobile/detail-of-immobile/detail-of-immobile.component.ts
@@ -1,5 +1,8 @@
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { Component, OnInit } from '@angular/core';
-
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { ptBrLocale } from 'ngx-bootstrap/locale';
+defineLocale('pt-br', ptBrLocale);
 @Component({
   selector: 'app-detail-of-immobile',
   templateUrl: './detail-of-immobile.component.html',
@@ -11,9 +14,10 @@ export class DetailOfImmobileComponent implements OnInit {
   maxDate = new Date();
   selectedServices: string[] = [];
 
-  constructor() {
+  constructor(localeService: BsLocaleService) {
     this.maxDate.setDate(this.maxDate.getDate() + 7);
     this.bsInlineRangeValue = [this.bsInlineValue, this.maxDate];
+    localeService.use('pt-br')
   }
 
   ngOnInit(): void {
